f6d8550f402e908f34a2b6b61f30515c62b8496b
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2016-06-28 12:49:36 -0700

    Fixes a name in functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'wsu_exlpre_enque_scripts');
 
 function wsu_explore_enque_scripts(){
 
-		wp_enqueue_script( 'wsu-labs-ie-polyfill', get_stylesheet_directory_uri() . '/js/polyfill.js', array( 'jquery' ), spine_get_script_version(), false );
+		wp_enqueue_script( 'wsu-explore-ie-polyfill', get_stylesheet_directory_uri() . '/js/polyfill.js', array( 'jquery' ), spine_get_script_version(), false );
 }
 /*
 * Add HTML5 search box on the side bar menu
