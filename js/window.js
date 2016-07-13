f2ac3e42cd764ad2d37a48582318b53e1b26e576
Phil Cable <pcable@wsu.edu>
2016-07-13 09:02:20 -0700

    Add function to toggle `menu-open` when nav items are clicked and tidy up adjust spacing throughout

--- js/window.js
+++ js/window.js
@@ -1,7 +1,22 @@
 /* Hamburger menu */
-jQuery("a.hamburger").click(function(){
-        jQuery("a.hamburger").toggleClass("menu-open");
-    });
+(function($) {
+	$('.hamburger').click(function(){
+		$('a.hamburger').toggleClass('menu-open');
+	});
+	$('.site-nav').on('click', 'a', function() {
+		var target = $(this.hash),
+			burger = $(this).closest('ul').prev('.hamburger');
+
+		if (target.length) {
+			burger.toggleClass('menu-open');
+			$('html, body').animate({
+				scrollTop: target.offset().top - 90
+			}, 500);
+			return false;
+		}
+	});
+}(jQuery));
+
 (function($,window) {
 	var check_sticky = function() {
 		var $sticky_images = $('.stick-image');
@@ -23,10 +38,10 @@ jQuery("a.hamburger").click(function(){
 }(jQuery,window));
 
 /* JS Reveal */
-  window.sr = ScrollReveal();
+window.sr = ScrollReveal();
 
-      // Add class to <html> if ScrollReveal is supported
-      if (sr.isSupported()) {
-        document.documentElement.classList.add('sr');
-      }
-  sr.reveal('.icon-wrap');
+// Add class to <html> if ScrollReveal is supported
+if (sr.isSupported()) {
+	document.documentElement.classList.add('sr');
+}
+sr.reveal('.icon-wrap');
